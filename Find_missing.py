904e9da6807d363b783dccc28ea7f071fdc47c76
rohitgade9890 <rohitarungade@gmail.com>
2025-09-14 12:02:29 +0530

    find missing solved check and merge

--- Find_missing.py
+++ Find_missing.py
@@ -1,1 +1,9 @@
 #1. Find the missing number in an array of 1…n integers.
+arr=[1,2,3,5,6]
+def find_missing(arr):
+    summ=sum(arr)
+    actual_summ=0
+    for x in range(1,len(arr)+2):
+        actual_summ = actual_summ + x
+    return actual_summ - summ
+print(find_missing(arr))
