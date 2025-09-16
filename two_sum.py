63dfc6575e04a79727292e9d2686549b93c17e78
rohitgade9890 <rohitarungade@gmail.com>
2025-09-16 17:41:28 +0530

    adding indesex method

--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,5 @@
 #check wether sum of two numbers from array makes target return true or false
+#bruteforce
 arr=[2,34,2,53,2,4,7,8,4]
 target=8
 def two_sum(arr,target):
@@ -7,4 +8,19 @@ def two_sum(arr,target):
             if arr[x] + arr[y] == target:
                 return True
     return False
+print(two_sum(arr,target))
+
+
+
+#check wether sum of two numbers from array makes target return indexes
+#bruteforce
+arr=[2,34,2,53,2,4,7,8,4]
+target=8
+def two_sum(arr,target):
+    res=[]
+    for x in range(len(arr)):
+        for y in range(x+1,len(arr)):
+            if arr[x] + arr[y] == target:
+                res.append([x,y])
+    return res
 print(two_sum(arr,target))
