6b9c7976d9a74cf20797bc6c4788bd75e85280bc
rohitgade9890 <rohitarungade@gmail.com>
2025-09-17 17:14:30 +0530

    new algo dutch national flag

--- sort_array_of_012.py
+++ sort_array_of_012.py
@@ -22,3 +22,22 @@ def sortt(arr):
 print(sortt(arr))
 
 #optimized
+#sort arrays of 0's 1's and 2's
+#using dutch national flag algo
+arr=[0,0,2,1,1,2,0,1,2]
+def sort(arr):
+    low=0
+    mid=0
+    high=len(arr)-1
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low],arr[mid]=arr[mid],arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid],arr[high]=arr[high],arr[mid]
+            high -= 1
+    return arr
+print(sort(arr))
