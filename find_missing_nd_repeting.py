9644876e47a33ee1ff9e53d81218baab2681acb1
rohitgade9890 <rohitarungade@gmail.com>
2025-10-04 18:57:14 +0530

    new code

--- find_missing_nd_repeting.py
+++ find_missing_nd_repeting.py
@@ -18,3 +18,26 @@ def mis_rep(arr):
             miss = arr[x] - 1
     return [miss,rep]
 print(mis_rep(arr))
+
+
+#to be learned
+arr = [4,3,6,2,1,1]
+
+def mis_rep(arr):
+    n = len(arr)
+    S = sum(arr)
+    P = sum(x*x for x in arr)
+    S_n = n*(n+1)//2
+    P_n = n*(n+1)*(2*n+1)//6
+
+    diff = S - S_n           # rep - miss
+    diff_sq = P - P_n        # rep^2 - miss^2
+
+    sum_rep_miss = diff_sq // diff   # rep + miss
+
+    rep = (diff + sum_rep_miss)//2
+    miss = sum_rep_miss - rep
+
+    return [miss, rep]
+
+print(mis_rep(arr))  # [5, 1]
