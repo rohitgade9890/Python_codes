1525b48bfd718b7d0bc36a5622fb7986de6a3bd7
rohitgade9890 <rohitarungade@gmail.com>
2025-10-05 13:21:52 +0530

    new

--- BinarySerch/binary_serch_02.py
+++ BinarySerch/binary_serch_02.py
@@ -1,6 +1,6 @@
 #find lower bound using binary serch
 #u have to find index of number which is closest or closest greter than target no
-arr=[2,5,6,7,8,13,14,15,18]
+arr=[2,5,12,12,12,12,12,18]
 target=12 #>=12
 #ans = 6  #no = 15
 def lower_bound(arr,target):
@@ -18,3 +18,21 @@ def lower_bound(arr,target):
             ans = mid
     return ans
 print(lower_bound(arr,target))
+
+arr = [2,5,12,12,12,12,12,18]
+target = 12
+
+def lower_bound(arr, target):
+    start = 0
+    end = len(arr) - 1
+    ans = len(arr)         # standard "not found" sentinel for lower_bound
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] >= target:
+            ans = mid      # possible answer, try to find a smaller index
+            end = mid - 1
+        else:
+            start = mid + 1
+    return ans
+
+print(lower_bound(arr, target))
