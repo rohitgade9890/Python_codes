d7f69208c68f76807e637423c59095fb5c1b6b60
rohitgade9890 <rohitarungade@gmail.com>
2025-10-05 11:05:56 +0530

    new code

--- BinarySerch/binary_serch_01.py
+++ BinarySerch/binary_serch_01.py
@@ -1,19 +1,17 @@
-#binary serch lecture 1
-#we implement binary serch to reduct serching time if array is sorted
-#find index where number appers in arr
-arr=[3,4,6,7,9,12,16,17]
-target=4
-def find_num(arr,k):
+#find index where number is present in list using binary serch
+arr=[2,5,6,7,8,11,15,18]
+target=11
+def find_num(arr):
+    arr.sort()
     start=0
     end=len(arr)-1
-    
     while start <= end:
-        mid=(start+end)//2
-        if arr[mid] == k:
+        mid=(start+end) // 2
+        if arr[mid] == target:
             return mid
-        elif arr[mid] > k:
-            end=mid-1
+        elif arr[mid] < target:
+            start = mid + 1
         else:
-            start=mid+1
+            end = mid - 1
     return -1
-print(find_num(arr,target))
+print(find_num(arr))
