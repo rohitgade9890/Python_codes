8db018bf5081c4db813d63a65529e51d217fb506
rohitgade9890 <rohitarungade@gmail.com>
2025-10-05 11:48:26 +0530

    new code

--- BinarySerch/binary_serch_01.py
+++ BinarySerch/binary_serch_01.py
@@ -1,17 +1,38 @@
-#find index where number is present in list using binary serch
-arr=[2,5,6,7,8,11,15,18]
-target=11
-def find_num(arr):
-    arr.sort()
+#binary serch lecture 1
+#we implement binary serch to reduct serching time if array is sorted
+#find index where number appers in arr
+arr=[3,4,6,7,9,12,16,17]
+target=4
+def find_num(arr,k):
     start=0
     end=len(arr)-1
+    
     while start <= end:
-        mid=(start+end) // 2
+        mid=(start+end)//2
+        if arr[mid] == k:
+            return mid
+        elif arr[mid] > k:
+            end=mid-1
+        else:
+            start=mid+1
+    return -1
+print(find_num(arr,target))
+
+#find index where number is present in list using binary serch
+#recursive method
+arr=[2,5,6,7,8,11,15,18]
+target=12
+start=0
+end=len(arr)-1
+def find_num_recursive(arr,start,end,target):
+    if start > end:
+        return -1
+    else:
+        mid = (start+end)//2
         if arr[mid] == target:
             return mid
         elif arr[mid] < target:
-            start = mid + 1
+            return find_num_recursive(arr,mid+1,end,target)
         else:
-            end = mid - 1
-    return -1
-print(find_num(arr))
+            return find_num_recursive(arr,start,mid-1,target)
+print(find_num_recursive(arr,start,end,target))
